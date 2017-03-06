d68c79fd391ca0ff2d2d820182ffa2da12e21234
Jesús López <inyenia@gmail.com>
2017-03-06 21:13:31 +0100

    update ipa resign

--- ios-ipa-resign.sh
+++ ios-ipa-resign.sh
@@ -74,27 +74,6 @@ function app_backup()
   echo "copy $app in $backup"
 }
 
-function ipa_info()
-{
-  echo "copy $1"
-  cp -P $1 ./info-app.ipa
-  unzip info-app.ipa
-
-  expirationDate=`/usr/libexec/PlistBuddy -c 'Print DeveloperCertificates:0' /dev/stdin <<< $(security cms -D -i Payload/*.app/embedded.mobileprovision) | openssl x509 -inform DER -noout -enddate | sed -e 's#notAfter=##'`
-
-  certificateSubject=`/usr/libexec/PlistBuddy -c 'Print DeveloperCertificates:0' /dev/stdin <<< $(security cms -D -i Payload/*.app/embedded.mobileprovision) | openssl x509 -inform DER -noout -subject`
-
-  cert_uid=`echo $certificateSubject | cut -d \/ -f 2 | cut -d \= -f 2`
-  cert_o=`echo $certificateSubject | cut -d \/ -f 4 | cut -d \= -f 2`  certificateSubject="$cert_o ($cert_uid)"
-
-  expirationMobileProvision=`/usr/libexec/PlistBuddy -c 'Print ExpirationDate' /dev/stdin <<< $(security cms -D -i Payload/*.app/embedded.mobileprovision)`
-
-  uuidMobileProvision=`/usr/libexec/PlistBuddy -c 'Print UUID' /dev/stdin <<< $(security cms -D -i Payload/*.app/embedded.mobileprovision)`
-
-  rm -rf Payload
-  rm info-app.ipa
-}
-
 if [ "$#" -lt 3 ]; then
     show_usage
 else
@@ -102,11 +81,5 @@ else
   echo "ReSign App $app"
   app_backup
   resign_app
-  echo "App Info"
-  ipa_info $app
   echo
-  echo "Firmado por: $certificateSubject"
-  echo "Certificado de distribución válido hasta: $expirationDate"
-  echo "Mobile Provision UUID: $uuidMobileProvision"
-  echo "Mobile Provision válido hasta: $expirationMobileProvision"
 fi
