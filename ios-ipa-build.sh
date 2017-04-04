f3c4dea32f2629b0b592a3177c2e54e48ec34ff6
Jesús López <inyenia@gmail.com>
2017-04-04 10:20:59 +0200

    fix build error with xcode 8.3

--- ios-ipa-build.sh
+++ ios-ipa-build.sh
@@ -21,6 +21,7 @@ certificate=${5}
 app_plist=${6}
 mobileprovision=${7}
 releases_dir=${8}
+export_options=${9}
 
 #devired_data_path="/tmp/DerivedData"
 devired_data_path="$releases_dir/DerivedData"
@@ -69,100 +70,76 @@ function set_environment()
   echo "$bundle_identifier ($scheme) version $short_version_string"
 }
 
-function build_app()
+function archive_app()
 {
-  #build the app
-  echo "Running xcodebuild ..."
+  echo "Archive as \"$certificate\", embedding provisioning profile $mobileprovision ..."
 
-  xcodebuild -workspace "$workspace" -scheme "$scheme" -sdk iphoneos -resource-rules='$(SDKROOT)/ResourceRules.plist' -configuration Release -derivedDataPath "$devired_data_path" clean build >| xcodebuild_output
+  xcodebuild -workspace $workspace -scheme $scheme -sdk "iphoneos" -configuration Distribution CODE_SIGN_IDENTITY="$certificate" PROVISIONING_PROFILE="$mobileprovision" OTHER_CODE_SIGN_FLAGS="--keychain $keychain" -archivePath app.xcarchive archive >| output
 
   if [ $? -ne 0 ]
   then
-    cat xcodebuild_output
-    failed xcodebuild
+    cat output
+    failed xcodebuild_archive
   fi
 
-  rm -rf xcodebuild_output
+  rm -rf output
+}
 
-  #get the name of the workspace to be build, used as the prefix of the DerivedData directory for this build
-  local workspace_name=$(echo "$workspace" | sed -n 's/\([^\.]\{1,\}\)\.xcworkspace/\1/p')
+function export_ipa()
+{
+  echo "Export as \"$certificate\", embedding provisioning profile $mobileprovision ..."
 
-  #locate this project's DerivedData directory
-  local project_derived_data_directory=$(grep -oE "$workspace_name-([a-zA-Z0-9]+)[/]" xcodebuild_output | sed -n "s/\($workspace_name-[a-z]\{1,\}\)\//\1/p" | head -n1)
-  local project_derived_data_path="$devired_data_path/$project_derived_data_directory"
-  #locate the .app file
+  xcodebuild -exportArchive -archivePath app.xcarchive -exportPath $releases_dir  -exportOptionsPlist $export_options
+  PROVISIONING_PROFILE_SPECIFIER="$mobileprovision" >| output
 
-  #  infer app name since it cannot currently be set using the product name, see comment above
-  #  project_app="$product_name.app"
-  project_app=$(ls -1 "$project_derived_data_path/Build/Products/Release-iphoneos/" | grep ".*\.app$" | head -n1)
-
-  if [ $(ls -1 "$project_derived_data_path/Build/Products/Release-iphoneos/" | grep ".*\.app$" | wc -l) -ne 1 ]
+  if [ $? -ne 0 ]
   then
-    echo "Failed to find a single .app build product."
-    # echo "Failed to locate $project_derived_data_path/Build/Products/Release-iphoneos/$project_app"
-    failed locate_built_product
+    cat output
+    failed xcodebuild_export
   fi
 
-  #copy app and dSYM files to the working directory
-  cp -Rf "$project_derived_data_path/Build/Products/Release-iphoneos/$project_app" $releases_dir
-  cp -Rf "$project_derived_data_path/Build/Products/Release-iphoneos/$project_app.dSYM" $releases_dir
-
-  #rename app and dSYM so that multiple environments with the same product name are identifiable
-  #echo "Retrieving build products..."
-  #rm -rf $project_dir/$bundle_identifier.app
-  #rm -rf $project_dir/$bundle_identifier.app.dSYM
-  #mv -f "$project_dir/$project_app" "$project_dir/$bundle_identifier.app"
-
-  #echo "$project_dir/$bundle_identifier.app"
-  #mv -f "$project_dir/$project_app.dSYM" "$project_dir/$bundle_identifier.app.dSYM"
-  #echo "$project_dir/$bundle_identifier.app.dSYM"
-  #project_app=$bundle_identifier.app
-
-  #relink CodeResources, xcodebuild does not reliably construct the appropriate symlink
-  #rm "$project_app/CodeResources"
-  #ln -s "$project_app/_CodeSignature/CodeResources" "$project_app/CodeResources"
+  rm -rf output
 }
 
-function sign_app()
+function check_ipa()
 {
-  echo "Codesign as \"$certificate\", embedding provisioning profile $mobileprovision"
-  #sign build for distribution and package as a .ipa
-  xcrun -sdk iphoneos PackageApplication "$releases_dir/$project_app" -o "$releases_dir/app.ipa" --sign "$certificate" --embed "$mobileprovision" || failed codesign
-}
+  echo "Checking $releases_dir/$scheme-$short_version_string.ipa ..."
 
-function verify_app()
-{
-  #verify the resulting app
-  #codesign -d --ent :- "$releases_dir/$project_app"
-  codesign -d -vvv --file-list - "$releases_dir/$project_app" || failed verification
-  mv $releases_dir/app.ipa $releases_dir/$scheme-$short_version_string.ipa
-  rm -rf $releases_dir/$project_app
-  rm -rf $releases_dir/$project_app.dSYM
+  cp $releases_dir/$scheme.ipa $releases_dir/$scheme-$short_version_string.ipa
+
+  mv $releases_dir/$scheme.ipa $releases_dir/$scheme.ipa
+  unzip $releases_dir/$scheme.ipa -d $releases_dir/
+  xcrun codesign -dv $releases_dir/Payload/documents.app >| output
+
+  if [ $? -ne 0 ]
+  then
+    cat output
+    failed xcodebuild_export
+  fi
+
+  rm -rf output
+  rm -rf app.xcarchive
+  rm -rf $releases_dir/Payload
 }
 
 echo "........ Validate Keychain ........"
 echo
 validate_keychain
 echo
-#echo
-#describe_sdks
-#echo
-#echo "........ Describe Workspace ........"
-#echo
-#describe_workspace
-#echo
 echo "........ Set Environment ........"
 echo
 set_environment
 echo
-echo "........ Build ........"
+echo "........ Archive ........"
 echo
-build_app
+archive_app
 echo
-echo "........ Package Application ........"
+echo "........ Export ........"
 echo
-sign_app
+export_ipa
 echo
-echo "........ Verify ........"
+echo "........ Check ........"
 echo
-verify_app
+check_ipa
+echo
+echo "......................."
