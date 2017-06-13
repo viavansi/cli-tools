3f14c373b7fb77f3fb11244cd65fa95b35b7a4f9
Jesús López <inyenia@gmail.com>
2017-06-13 12:46:29 +0200

    new git changelog script

---
+++ changelog.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FILE="History.md"
+CURRENT_DIR=`pwd`
+PREVIOUS_TAG=`git rev-list --max-parents=0 HEAD`
+
+echo "Creating changelog for project in $CURRENT_DIR"
+git pull --tags
+
+rm $FILE
+echo "" >> $FILE
+
+for TAG in `git tag --sort=v:refname|sed 's/\*//g'`;
+do 
+  echo "" | cat - $FILE > /tmp/out && mv /tmp/out $FILE
+
+  git log --no-merges --date=short --pretty=format:"  * %h - %ad - (%aN) %s" $PREVIOUS_TAG...$TAG | cat - $FILE > /tmp/out &&
+  mv /tmp/out $FILE
+
+  DATE=`git log -1 --format=%ad --date=short $TAG`
+  echo "===================" | cat - $FILE > /tmp/out && mv /tmp/out $FILE
+  echo "$TAG / $DATE" | cat - $FILE > /tmp/out && mv /tmp/out $FILE
+  echo "" | cat - $FILE > /tmp/out && mv /tmp/out $FILE
+      
+  PREVIOUS_TAG=$TAG
+done
+
+
+
